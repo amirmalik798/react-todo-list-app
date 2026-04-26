51c3406892ad3d159bc5a9413f35c6d0b4cbe410
amirmalik798 <exception.handler.798@gmail.com>
2026-04-26 13:09:19 +0200

    Update TaskItem.jsx

--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -4,12 +4,17 @@ function TaskItem({task, deleteTask, toggleTask}) {
 
     return (
         <li className={`todo-item ${task.completed ? "completed" : ""}`}>
-            <span className="todo-text">{task.taskTitle}</span>
-            <span className='todo-text'>{task.taskDate}</span>
             <div className="actions">
                 <button className="btn complete-btn" onClick={() => {
                     toggleTask(task)
                 }}>✔</button>
+            </div>
+            <div className="todo-content">
+                <span className="todo-text">{task.taskTitle}</span>
+                <span className='todo-date'>{task.taskDate}</span>
+            </div>
+            
+            <div className="actions">
                 <button className="btn delete-btn"
                 onClick={() => deleteTask(task)}>✖</button>
             </div>
